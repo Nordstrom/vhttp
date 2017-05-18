8861cb68294592e18b87717334f3c2b16b722394
Tom Wallace <Tom.Wallace@nordstrom.com>
2017-05-18 13:53:41 -0700

    Added ctrace support.

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 'use strict';
 
 var _ = require('lodash'),
-    flow = require('lodash/fp/flow'),
-    toPairs = require('lodash/fp/toPairs'),
-    fromPairs = require('lodash/fp/fromPairs'),
-    map = require('lodash/fp/map'),
-    defaults = require('lodash/fp/defaults'),
-    Promise = require('bluebird'),
-    glob = Promise.promisify(require('glob')),
-    readFile = Promise.promisify(require('fs').readFile),
-    XmlParser = require('xml2js').Parser,
-    xmlParser = Promise.promisifyAll(new XmlParser({ explicitRoot: true, explicitArray: false, mergeAttrs: true })),
-    fromXml = xmlParser.parseStringAsync,
-    path = require('path'),
-    querystring = require('querystring'),
-    url = require('url'),
-    sub = require('substituter'),
-    eql = require('smart-eql'),
-    request = require('request-promise'),
-    _verboseHandlers = {
-        send: function (opts) {
-            console.log('SEND%s: %s %s', opts.virtual ? ' [' + opts.virtual + ']' : '', opts.method, opts.uri);
-        },
-        sent: function (opts) {
-            console.log('SENT%s: %s %s [%s ms]', opts.virtual ? ' [' + opts.virtual + ']' : '', opts.method, opts.uri, opts.duration);
-        },
-        error: function (opts) {
-            console.log('ERROR%s: %s %s [%s ms]:', opts.virtual ? ' [' + opts.virtual + ']' : '', opts.method, opts.uri, opts.duration, (opts.error.error || opts.error));
-        },
-        debug: function (opts) {
-            console.log('DEBUG%s: %s %s:', opts.virtual ? ' [' + opts.virtual + ']' : '', opts.method, opts.uri, opts.message);
-        }
-    },
-    // _eventHandlers = _verbosetHandlers,
-    _root = path.resolve(__dirname + '/virtual'),
-    _scenarios = {},
-    _eventHandlers;
+  flow = require('lodash/fp/flow'),
+  toPairs = require('lodash/fp/toPairs'),
+  fromPairs = require('lodash/fp/fromPairs'),
+  map = require('lodash/fp/map'),
+  defaults = require('lodash/fp/defaults'),
+  Promise = require('bluebird'),
+  glob = Promise.promisify(require('glob')),
+  readFile = Promise.promisify(require('fs').readFile),
+  XmlParser = require('xml2js').Parser,
+  xmlParser = Promise.promisifyAll(new XmlParser({ explicitRoot: true, explicitArray: false, mergeAttrs: true })),
+  fromXml = xmlParser.parseStringAsync,
+  path = require('path'),
+  querystring = require('querystring'),
+  url = require('url'),
+  sub = require('substituter'),
+  eql = require('smart-eql'),
+  request = require('request-promise'),
+  _verboseHandlers = {
+      send: function (opts) {
+          console.log('SEND%s: %s %s', opts.virtual ? ' [' + opts.virtual + ']' : '', opts.method, opts.uri);
+      },
+      sent: function (opts) {
+          console.log('SENT%s: %s %s [%s ms]', opts.virtual ? ' [' + opts.virtual + ']' : '', opts.method, opts.uri, opts.duration);
+      },
+      error: function (opts) {
+          console.log('ERROR%s: %s %s [%s ms]:', opts.virtual ? ' [' + opts.virtual + ']' : '', opts.method, opts.uri, opts.duration, (opts.error.error || opts.error));
+      },
+      debug: function (opts) {
+          console.log('DEBUG%s: %s %s:', opts.virtual ? ' [' + opts.virtual + ']' : '', opts.method, opts.uri, opts.message);
+      }
+  },
+  // _eventHandlers = _verbosetHandlers,
+  _root = path.resolve(__dirname + '/virtual'),
+  _scenarios = {},
+  _eventHandlers;
 
 if (!RegExp.prototype.toJSON) {
     // this allows stringify to work on regular expresssions.
@@ -45,22 +45,22 @@ if (!RegExp.prototype.toJSON) {
 
 function initCall(call) {
     let key = call[0],
-        value = call[1],
-        parts = key.split(':'),
-        name = parts[0],
-        num = parts[1],
+      value = call[1],
+      parts = key.split(':'),
+      name = parts[0],
+      num = parts[1],
 
-        files = glob.sync(_root + '/**/' + name + '.*'),
+      files = glob.sync(_root + '/**/' + name + '.*'),
 
-        request = {
-            uri: value.uri,
-            method: value.method,
-            qs: value.qs
-        },
-        response = {
-            status: value.status,
-            delay: value.delay
-        };
+      request = {
+          uri: value.uri,
+          method: value.method,
+          qs: value.qs
+      },
+      response = {
+          status: value.status,
+          delay: value.delay
+      };
 
     _.forEach(files, function (file) {
         if (_.endsWith(file, `.request.${num}.json`)) response.json = file;
@@ -84,20 +84,20 @@ function initCall(call) {
 
 function initScenario(scenario) {
     let key = scenario[0],
-        value = scenario[1],
-        calls = flow(
-            toPairs,
-            map(initCall)
-        )(value);
+      value = scenario[1],
+      calls = flow(
+        toPairs,
+        map(initCall)
+      )(value);
     return [key, calls];
 }
 
 function renderBody(obj, raw) {
     if (obj.json) {
         return readFile(obj.json, 'utf8')
-            .then(function (data) {
-                return JSON.parse(data);
-            });
+          .then(function (data) {
+              return JSON.parse(data);
+          });
     }
 
     if (obj.templateJson) {
@@ -107,19 +107,19 @@ function renderBody(obj, raw) {
     }
     if (obj.xml) {
         return readFile(obj.xml, 'utf8')
-            .then(function (xml) {
-                return raw ? xml.toString() : fromXml(xml);
-            });
+          .then(function (xml) {
+              return raw ? xml.toString() : fromXml(xml);
+          });
     }
 
     if (obj.templateXml) {
         return readFile(obj.templateXml, 'utf8').then(function (xml) {
-                return raw
-                    ? sub(xml, _.isFunction(obj.data) ? obj.data() : obj.data)
-                    : fromXml(xml).then(function (xmlObj) {
-                    return sub(xmlObj, _.isFunction(obj.data) ? obj.data() : obj.data);
-                });
-            }
+              return raw
+                ? sub(xml, _.isFunction(obj.data) ? obj.data() : obj.data)
+                : fromXml(xml).then(function (xmlObj) {
+                  return sub(xmlObj, _.isFunction(obj.data) ? obj.data() : obj.data);
+              });
+          }
         );
     }
 
@@ -147,8 +147,8 @@ function render(scenario) {
 
 function prepareBody(opts) {
     return _.isString(opts.body)
-        ? fromXml(opts.body)
-        : Promise.resolve(opts.body);
+      ? fromXml(opts.body)
+      : Promise.resolve(opts.body);
 }
 
 function prepareUriObj(opts) {
@@ -166,26 +166,30 @@ function prepareUriObj(opts) {
 }
 
 class Vhttp {
-    constructor(virtual) {
+    constructor(virtual, ctraceRequest) {
         this.virtual = virtual;
+        if (ctraceRequest) {
+            this.ctraceRequest = ctraceRequest;
+            ctraceRequest.init(request);
+        }
     }
 
     init() {
         let self = this;
         if (this.scenario) return Promise.resolve();
         return (render(_scenarios[this.virtual]) || Promise.resolve())
-            .then(function (scenario) {
-                self.scenario = self.scenario || scenario;
-            });
+          .then(function (scenario) {
+              self.scenario = self.scenario || scenario;
+          });
     }
 
     _findCall(opts) {
         return _.find(this.scenario, function (item) {
             let call = item.value,
-                callReq = call.request,
-                qs = opts.qs,
-                method = _.trim(opts.method.toUpperCase()),
-                uri = _.trim(opts.uri.toLowerCase());
+              callReq = call.request,
+              qs = opts.qs,
+              method = _.trim(opts.method.toUpperCase()),
+              uri = _.trim(opts.uri.toLowerCase());
 
             if (!call._initialized) {
                 callReq.method = callReq.method.toUpperCase();
@@ -204,29 +208,29 @@ class Vhttp {
             ];
 
             opts.message = 'Matching to ' + callReq.method + ':' + callReq.uri +
-                (callReq.qs ? ('?' + querystring.stringify(callReq.qs)) : '') +
-                ' - method:' + eq[0] +
-                '; uri:' + eq[1] +
-                '; qs:' + eq[2] +
-                '; body:' + eq[3];
+              (callReq.qs ? ('?' + querystring.stringify(callReq.qs)) : '') +
+              ' - method:' + eq[0] +
+              '; uri:' + eq[1] +
+              '; qs:' + eq[2] +
+              '; body:' + eq[3];
             if (_eventHandlers && _eventHandlers.debug) {
                 _eventHandlers.debug(opts);
             }
 
             if (eq[0] && eq[1] && !eq[2]) {
                 opts.error = new Error(
-                    'Query strings do not match for ' + method + ':' + uri +
-                    '\nEXPECTED\n' + JSON.stringify(callReq.qs, null, 4) +
-                    '\nACTUAL\n' + JSON.stringify(qs, null, 4));
+                  'Query strings do not match for ' + method + ':' + uri +
+                  '\nEXPECTED\n' + JSON.stringify(callReq.qs, null, 4) +
+                  '\nACTUAL\n' + JSON.stringify(qs, null, 4));
                 if (_eventHandlers && _eventHandlers.error) {
                     _eventHandlers.error(opts);
                 }
             }
             if (eq[0] && eq[1] && !eq[3]) {
                 opts.error = new Error(
-                    'Bodies do not match for ' + method + ':' + uri +
-                    '\nEXPECTED\n' + JSON.stringify(callReq.body, null, 4) +
-                    '\nACTUAL\n' + JSON.stringify(opts.preparedBody, null, 4));
+                  'Bodies do not match for ' + method + ':' + uri +
+                  '\nEXPECTED\n' + JSON.stringify(callReq.body, null, 4) +
+                  '\nACTUAL\n' + JSON.stringify(opts.preparedBody, null, 4));
                 if (_eventHandlers && _eventHandlers.error) {
                     _eventHandlers.error(opts);
                 }
@@ -237,24 +241,28 @@ class Vhttp {
     }
 
     _sendReal(opts) {
-        return request(opts)
-            .then(function (data) {
-                if (_eventHandlers && _eventHandlers.sent) {
-                    opts.duration = Date.now() - opts.startedAt;
-                    opts.responseBody = data;
-                    _eventHandlers.sent(opts);
-                }
-                return data;
-            })
-            .catch(function (err) {
-                if (_eventHandlers && _eventHandlers.error) {
-                    opts.duration = Date.now() - opts.startedAt;
-                    opts.error = err.error || err;
-                    opts.statusCode = err.response && err.response.statusCode;
-                    _eventHandlers.error(opts);
-                }
-                throw err;
-            });
+        var requestLibrary = request;
+        if (this.ctraceRequest) {
+            requestLibrary = this.ctraceRequest;
+        }
+        return requestLibrary(opts)
+          .then(function (data) {
+              if (_eventHandlers && _eventHandlers.sent) {
+                  opts.duration = Date.now() - opts.startedAt;
+                  opts.responseBody = data;
+                  _eventHandlers.sent(opts);
+              }
+              return data;
+          })
+          .catch(function (err) {
+              if (_eventHandlers && _eventHandlers.error) {
+                  opts.duration = Date.now() - opts.startedAt;
+                  opts.error = err.error || err;
+                  opts.statusCode = err.response && err.response.statusCode;
+                  _eventHandlers.error(opts);
+              }
+              throw err;
+          });
     }
 
     _sendVirtual(opts) {
@@ -274,32 +282,32 @@ class Vhttp {
         call = call.value;
 
         let status = call.response.status || 200,
-            delay = call.response.delay || 0,
-            responseBody = call.response.body;
+          delay = call.response.delay || 0,
+          responseBody = call.response.body;
 
         if (!status || /^2/.test(status)) {
             return Promise
-                .delay(delay)
-                .then(function () {
-                    if (_eventHandlers && _eventHandlers.sent) {
-                        opts.duration = Date.now() - opts.startedAt;
-                        opts.responseBody = responseBody;
-                        _eventHandlers.sent(opts);
-                    }
-                    return responseBody;
-                });
+              .delay(delay)
+              .then(function () {
+                  if (_eventHandlers && _eventHandlers.sent) {
+                      opts.duration = Date.now() - opts.startedAt;
+                      opts.responseBody = responseBody;
+                      _eventHandlers.sent(opts);
+                  }
+                  return responseBody;
+              });
         }
 
         return Promise
-            .delay(delay)
-            .then(function () {
-                if (_eventHandlers && _eventHandlers.error) {
-                    opts.duration = Date.now() - opts.startedAt;
-                    opts.error = responseBody;
-                    _eventHandlers.error(opts);
-                }
-                return Promise.reject({ error: responseBody });
-            });
+          .delay(delay)
+          .then(function () {
+              if (_eventHandlers && _eventHandlers.error) {
+                  opts.duration = Date.now() - opts.startedAt;
+                  opts.error = responseBody;
+                  _eventHandlers.error(opts);
+              }
+              return Promise.reject({ error: responseBody });
+          });
 
     }
 
@@ -309,35 +317,35 @@ class Vhttp {
         }
 
         let self = this,
-            virtual = this.virtual,
-            method = opts.method.toUpperCase(),
-            uri = opts.uri.toLowerCase();
+          virtual = this.virtual,
+          method = opts.method.toUpperCase(),
+          uri = opts.uri.toLowerCase();
 
         return this.init()
-            .then(function () {
-                return Promise.join(prepareUriObj(opts), prepareBody(opts), function (uriObj, body) {
-                    if (self.virtual && !self.scenario) {
-                        let err = new Error('No virtual ' + virtual + ' scenario found for ' + method + ':' + uri);
-                        if (_eventHandlers && _eventHandlers.error) {
-                            opts.error = err;
-                            _eventHandlers.error(opts);
-                        }
-                        throw err;
-                    }
+          .then(function () {
+              return Promise.join(prepareUriObj(opts), prepareBody(opts), function (uriObj, body) {
+                  if (self.virtual && !self.scenario) {
+                      let err = new Error('No virtual ' + virtual + ' scenario found for ' + method + ':' + uri);
+                      if (_eventHandlers && _eventHandlers.error) {
+                          opts.error = err;
+                          _eventHandlers.error(opts);
+                      }
+                      throw err;
+                  }
 
-                    opts.uri = uriObj.uri;
-                    opts.qs = uriObj.qs;
-                    opts.preparedBody = body;
-                    opts.virtual = virtual;
-                    if (_eventHandlers && _eventHandlers.send) {
-                        _eventHandlers.send(opts);
-                    }
+                  opts.uri = uriObj.uri;
+                  opts.qs = uriObj.qs;
+                  opts.preparedBody = body;
+                  opts.virtual = virtual;
+                  if (_eventHandlers && _eventHandlers.send) {
+                      _eventHandlers.send(opts);
+                  }
 
-                    return self.scenario
-                        ? self._sendVirtual(opts)
-                        : self._sendReal(opts);
-                });
-            });
+                  return self.scenario
+                    ? self._sendVirtual(opts)
+                    : self._sendReal(opts);
+              });
+          });
     }
 
     done() {
@@ -368,10 +376,10 @@ class Vhttp {
 
     static register(scenarios) {
         _scenarios = flow(
-            toPairs,
-            map(initScenario),
-            fromPairs,
-            defaults(_scenarios)
+          toPairs,
+          map(initScenario),
+          fromPairs,
+          defaults(_scenarios)
         )(scenarios);
     }
 
